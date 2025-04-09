ada5be4f0a503119b08f6839aac9a8bf15236129
md-cs0 <200961600+md-cs0@users.noreply.github.com>
2025-04-09 23:34:47 +0100

    Add test SDL code

--- src/main.c
+++ src/main.c
@@ -3,9 +3,60 @@
 */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <assert.h>
+#include <SDL.h>
 
-int main()
+#define NES_W   256
+#define NES_H   240
+
+int main(int argc, char** argv)
 {
-    puts("Hello world!");
+    SDL_Init(SDL_INIT_EVERYTHING);
+    srand((unsigned)time(NULL));
+
+    SDL_Window* window = SDL_CreateWindow("nesemu", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                                          640, 480, 0);
+    assert(window);
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    assert(renderer);
+
+    SDL_Texture* buffer = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ABGR8888, SDL_TEXTUREACCESS_STREAMING,
+                                            NES_W, NES_H);
+    assert(buffer);
+    
+    SDL_Colour grid[NES_H * NES_W];
+    for (int i = 0; i < sizeof(grid) / sizeof(grid[0]); ++i)
+    {
+        grid[i].a = 0;
+        grid[i].r = 0;
+        grid[i].g = rand() % 256; // Obviously don't do this in real code, due to modulo bias!
+        grid[i].b = rand() % 256;
+    }
+
+    for (;;)
+    {
+        SDL_Event event;
+        while (SDL_PollEvent(&event))
+        {
+            switch (event.type)
+            {
+            case SDL_QUIT:
+                goto exit;
+            }
+        }
+
+        SDL_RenderClear(renderer);
+        SDL_UpdateTexture(buffer, NULL, grid, NES_W * 4);
+        SDL_RenderCopy(renderer, buffer, NULL, NULL);
+        SDL_RenderPresent(renderer);
+    }
+
+exit:
+    SDL_DestroyTexture(buffer);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
     return 0;
 }
